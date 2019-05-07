ba99cccb9454b43016fb7a39cc0e2be466ef0083
Brian Tan <briantan37@gmail.com>
2019-05-07 02:56:16 -0700

    partially working double transposition

--- cipherutils.php
+++ cipherutils.php
@@ -55,6 +55,8 @@
         public function encrypt($input, $key) 
         {
 
+            $arr = $this->parseTo2DArray($input);
+            $this->swap($key, $arr, count($arr));
         }
 
         public function decrypt($input, $key)
@@ -62,20 +64,75 @@
 
         }
 
-        private function create2DArray($key) {
-            $key1 = explode(",", $key[0]);
-            $key2 = explode(",", $key[1]);
-            $arr = array_fill(0, count($key1),"1");
-            for($i = 0; $i < count($arr); $i++) {
-                $tmpArr = array_fill(0, count($key2), "2");
-                $arr[$i] = $tmpArr;
+        private function parseTo2DArray($input) {
+            $len = strlen($input);
+            //create two each factor for length and get the ceiling to not lose some values
+            $factors = ceil(sqrt($len));
+            //split the input to array of characters
+            $inputArr = str_split($input);
+            //create an index variable
+            $index = 0;
+
+            //the 2D array to output
+            $arr = array();
+            //Nested for loop that creates multiple arrays for the output array
+            for($i = 0; $i < $factors; $i++) {
+                $tmpArr = array();
+                //This inner loop parses each character from the input array and stores them in temporary array
+                for($j = 0; $j < $factors; $j++) {
+                    //Check if the index is not greater than the len of the input to not get OutOfBoundsException
+                    if($index < $len) {
+                        $tmpArr[] = $inputArr[$index];
+                        $index++;
+                    }
+                    //if it is, just insert space
+                    else {
+                        $tmpArr[] = " ";
+                    }
+
+                }
+                //Store the temporary array in the output array
+                $arr[] = $tmpArr;
+            }
+            return $arr;
+        }
+
+        private function swap($key, $arr, $len) {
+            $key1 = explode(",", $key[0]);      //key to switch the rows
+            $key2 = explode(",", $key[1]);      //key to switch the column;
+            $output = array();
+            $swapFlag = (count($key1) < $len) ? true : false;
+            //Start switching the rows
+            for($i = 0; $i < $len; $i++) {
+                //make sure that the value of i does not exceed the length of key1
+                if($i < count($key1)) {
+                    $keyIndex = (int) $key1[$i];
+                    if($swapFlag) {                       //if the key's index is less than the len of the array
+                        $output[$keyIndex] = $arr[$i];       //Replace the key index to the array's index
+                    }
+                    else {
+                        $output[$i] = $arr[$keyIndex];        //store the array of the key's index at the current index
+                    }
+                }
+
+            }
+            $swapFlag = (count($key2) < $len) ? true : false;
+            for($j = 0; $j < $len; $j++) {
+                if($j < count($key2)) {
+                    $keyIndex = (int) $key2[$i];
+                    if($swapFlag) {
+
+                    }
+                }
             }
         }
     }
 
     $dtrans = new DTransposition;
-    $key = array("1,2,3,4","2,4,5,3");
-    $dtrans->encrypt("BJKSA", $key);
+    //            c,e,f,a,b,
+    $key = array("2,4,5,0","2,4,5,3");
+    $input = "aaaaaabbbbbbccccccddddddeeeeeeffffff";
+    $dtrans->encrypt($input, $key);
     // define("RESET_ASCII_VALUE", 65);
     // $sub = new Substitution;
     // echo $sub->decrypt("USXHKWUHDXNETXUW", "UEOQDGYSXCFPHWVJRTNIAMBZKL");
